a6925c3da2144d1575ac40958f98ba841288f4f2
AYTC <aytacalir@gmail.com>
2024-09-10 01:01:21 +0300

    ssss

--- wednesday-react-router/src/component/Error/NotFount.jsx
+++ wednesday-react-router/src/component/Error/NotFount.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import "./NotFount"
+import not from "../../assets/img/project-2.jpg"
 
 const NotFount = () => {
   return (
-    <div>NotFount</div>
+    <div>
+      <img className='notfount' src={not} alt="" />
+    </div>
   )
 }
 
